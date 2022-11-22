9701b33ef5c1a45f0bdf6aa6efbfdc434495129d
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-22 13:54:13 +0530

    Last Name

--- User_Registration_Problem/Program.cs
+++ User_Registration_Problem/Program.cs
@@ -5,12 +5,7 @@
         static void Main(string[] args)
         {
             User_Registration user = new User_Registration();
-            //Console.Write("Enter First Name : ");
-            //string firstName = Console.ReadLine();
-            user.FirstNameAndLastName("Mahesh");
-            //Console.Write("Enter Email Id Given in Chart : ");
-            //string email = Console.ReadLine();
-            //user.SampleEmail(email);
+            user.UserInputName();
         }
     }
 }
