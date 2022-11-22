81fc77adfe46e6c65145a3fef031a98b96617c4d
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-22 15:00:18 +0530

    Validate Mobile Number

--- User_Registration_Problem/Program.cs
+++ User_Registration_Problem/Program.cs
@@ -5,8 +5,9 @@
         static void Main(string[] args)
         {
             User_Registration user = new User_Registration();
-            user.UserInputName();
-            user.Email();
+            //user.UserInputName();
+            //user.Email();
+            user.MobileNumber();
         }
     }
 }
