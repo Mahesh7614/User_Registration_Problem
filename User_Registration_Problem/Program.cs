da23e24d53eedd31b4339f2be90a887d0e737e15
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-22 12:57:19 +0530

    Sample Emails

---
+++ User_Registration_Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace User_Registration_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Email Id Given in Chart : ");
+            string email = Console.ReadLine();
+            User_Registration.Pattern(email);
+        }
+    }
+}
---
+++ User_Registration_Problem/User_Registration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace User_Registration_Problem
+{
+    internal class User_Registration
+    {
+        public static void Pattern(string pattern)
+        {
+            Regex email = new Regex(@"^(abc)([_\+\-\.]{0,1}[a-zA-Z0-9])*[@][a-z0-9]{1,} *[.][a-z]{2,}[\.\,]{0,}[a-z]{0,}$");
+            bool matches = email.IsMatch(pattern);
+
+            if (matches == true)
+            {
+                Console.WriteLine("Email ID is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Email ID");
+            }
+        }
+    }
+}
