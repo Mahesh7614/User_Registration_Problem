7d4f11968eb139eb5d7a0b605fe3b19df314e587
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-22 15:33:33 +0530

    Password 8 Characters

--- User_Registration_Problem/User_Registration.cs
+++ User_Registration_Problem/User_Registration.cs
@@ -75,7 +75,7 @@ namespace User_Registration_Problem
         {
             Console.WriteLine("Enter Password Contains 8 Characters : ");
             string password = Console.ReadLine();
-            Regex pass = new Regex(@"^[a-zA-Z0-9]{8}$");
+            Regex pass = new Regex("^[A-za-z0-9]{8,}");
             bool matches = pass.IsMatch(password);
             if (matches == true)
             {
